7da4e7748f26b51c1415951d05bb0f26c655948d
Max Kyrchanov <max.kyrchanov@gmail.com>
2024-06-04 18:36:49 +0500

    complete v1.0

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
 import App from './App';
 
 // const url = "192.168.1.110:7070";
-const url = 'ahj-lesson8-task2-backend.onrender.com/'
+const url = 'ahj-lesson8-task2-backend.onrender.com'
 
 new App('#app', url);
 
